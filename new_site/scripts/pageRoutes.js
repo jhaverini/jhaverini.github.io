4fc2ed75ec78377de8ffb4baf1cae7c33582f5ea
kleydon <kleydon@users.noreply.github.com>
2024-05-11 17:51:26 -0700

    V3 - Starting

---
+++ new_site/scripts/pageRoutes.js
@@ -0,0 +1,57 @@
+const pr = { //"pr": A containing namespace, to prevent cross-file variable/function name collisions
+
+  pages: {
+    home: {
+      name: "Home",
+      url: "./about.html",
+      target: "_self",
+    },
+    about: {
+      name: "About",
+      url: "./about.html",
+      target: "_self",
+    },
+    projects: {
+      name: "Projects",
+      url: "./projects.html",
+      target: "_self",
+    },
+    projectKaiserNicu: {
+      name: "Case Study: Kaiser NICU",
+      url: "./projects-kaiser-nicu.html",
+      target: "_self",
+    },
+    projectKeyConservationApp: {
+      name: "Case Study: Key Conservation App",
+      url: "./projects-key-conservation-app.html",
+      target: "_self",
+    },
+    resume: {
+      name: "\u25F9" + " Resume",
+      url: "../pdfs/resume-jhaveri.pdf",
+      target: "_blank",
+    }
+  },
+  
+
+  currentPageFileName: () => {
+    const parts = document.location.href.split('/')
+    const lastPart = parts[parts.length - 1] //Page file name - plus any # anchor
+    return lastPart.split('#')[0]; //Remove any # anchor
+  },
+
+
+  pageFileName: (page) => {
+    const parts = page.url.split('/');
+    const lastPart = parts[parts.length - 1]; //Page file name - plus any # anchor
+    return lastPart.split('#')[0]; //Remove any # anchor
+  },
+
+
+  isCurrentPage: (page) => {
+    return pr.currentPageFileName().toLowerCase() === pr.pageFileName(page).toLowerCase();
+  },
+};
+
+
+
